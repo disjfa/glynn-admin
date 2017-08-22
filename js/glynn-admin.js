928bb3eddefd82119677897c15de6f7492498446
disjfa <disjfa@disjfa.nl>
2017-08-22 22:04:09 +0000

    Bumped to bootstrap 4 beta

--- js/glynn-admin.js
+++ js/glynn-admin.js
@@ -1,9 +1,8 @@
 var jQuery = require('jquery');
 window.$ = window.jQuery = jQuery;
 
-var Tether = require('tether');
-window.Tether = Tether;
-
+var Popper = require('popper.js');
+window.Popper = Popper;
 
 require('bootstrap/dist/js/bootstrap.js');
 require('chart.js');
